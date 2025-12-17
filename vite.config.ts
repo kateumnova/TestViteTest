7a29bd11182f43fd290f8798b9e6bb0243a5d9ab
Kate Umnova <umnovaea@gmail.com>
2025-12-17 22:11:35 +0300

    Add vite-plugin-pwa for CameraKit caching

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,39 @@
-// vite.config.js
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/(?:cf-st|bolt-gcdn)\.sc-cdn\.net/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'camera-kit-assets',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 24 * 60 * 60 * 7 // 1 week
+              },
+              cacheableResponse: { statuses: [0, 200] }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/.*\.snapar\.com/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'snapar-api',
+              expiration: { maxAgeSeconds: 60 * 60 } // 1 hour
+            }
+          }
+        ]
+      }
+    })
+  ],
   base: '/TestViteTest/'
 })
